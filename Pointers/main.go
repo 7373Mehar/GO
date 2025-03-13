416fb481b7e9cf714b5c12c79a572bb2c481dcad
Mehar <meharmutreja777@gmail.com>
2025-03-13 19:56:59 +0530

    Add appropriate comments

--- Pointers/main.go
+++ Pointers/main.go
@@ -5,18 +5,22 @@ import "fmt"
 func main() {
 
 	var ptr *int
+	//NIL Value as it is only initialized here.
 	fmt.Println("Value of pointer is: ", ptr)
 
 	newNum := 70
 	newPtr := &newNum
+	//Passing memory address.
 
-	fmt.Println("Value of new num: ", newNum)
-	fmt.Println("Value of new ptr: ", newPtr)
+	fmt.Println("Value of new num: ", newNum)                  //Should print 70
+	fmt.Println("Value of new ptr: ", newPtr)                  // Will print memory address
+	fmt.Println("Actual value at the memory address", *newPtr) //70
 
-	newPtr1 := *newPtr + 2
-	//*newPtr = *newPtr + 2
-	fmt.Println("New Value of ptr1: ", newPtr1)
-	fmt.Println("Value of newNum after update: ", newNum)
-	fmt.Println("Value of newPtr: ", *newPtr)
+	//newPtr1 := *newPtr + 2 // Will add 2 to the initial value.
+	//fmt.Println("New Value of ptr1: ", newPtr1)           //72
+
+	*newPtr = *newPtr + 2
+	fmt.Println("Value of newNum after update: ", newNum) //70
+	fmt.Println("Value of newPtr: ", *newPtr)             //72
 
 }
